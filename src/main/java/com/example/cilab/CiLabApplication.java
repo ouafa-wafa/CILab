8864fd57eb955032ae4dc7d4d22dc65c883f02b2
ouafa-wafa <wfoufou643@gmail.com>
2025-11-11 06:24:24 +0100

    Initial commit

---
+++ src/main/java/com/example/cilab/CiLabApplication.java
@@ -0,0 +1,19 @@
+package com.example.cilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class CiLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiLabApplication.class, args);
+    }
+    @RequestMapping
+    public String index() {
+        return "Hello ouafa!";
+    }
+}
